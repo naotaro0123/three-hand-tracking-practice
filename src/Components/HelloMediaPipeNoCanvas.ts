5d8b48aae4730ba60118792472220dd3934fd1c1
naotaro0123 <n.ikewada@gmail.com>
2020-03-16 23:25:21 +0900

    fix: add no canvas class

--- src/Components/HelloMediaPipeNoCanvas.ts
+++ src/Components/HelloMediaPipeNoCanvas.ts
@@ -1,10 +1,4 @@
-// reference:
-// https://github.com/tensorflow/tfjs-models/blob/master/handpose/demo/index.html
-// https://www.npmjs.com/package/@tensorflow-models/handpose
-// https://github.com/PAIR-code/scatter-gl
-
 import * as handpose from '@tensorflow-models/handpose';
-import { Coords3D } from '@tensorflow-models/handpose/dist/pipeline';
 import { ScatterGL, Point3D } from 'scatter-gl';
 
 const VIDEO_WIDTH = 640;
@@ -20,8 +14,6 @@ const fingerLookupIndices = {
 export default class HelloMediaPipe {
   private model: handpose.HandPose;
   private video: HTMLVideoElement;
-  private canvas: HTMLCanvasElement;
-  private context: CanvasRenderingContext2D;
   private scatterGL: ScatterGL;
   private scatterGLHasInitialized: boolean = false;
 
@@ -86,23 +78,6 @@ export default class HelloMediaPipe {
     const videoHeight = this.video.videoHeight;
     this.video.width = videoWidth;
     this.video.height = videoHeight;
-
-    this.canvas = document.createElement('canvas');
-    this.canvas.width = videoWidth;
-    this.canvas.height = videoHeight;
-    this.canvas.style.position = 'absolute';
-    this.canvas.style.top = '0px';
-    this.canvas.style.right = '0px';
-    this.canvas.style.zIndex = '5';
-    document.body.appendChild(this.canvas);
-
-    this.context = this.canvas.getContext('2d');
-    this.context.clearRect(0, 0, videoWidth, videoHeight);
-    this.context.strokeStyle = 'red';
-    this.context.fillStyle = 'red';
-    this.context.translate(this.canvas.width, 0);
-    this.context.scale(-1, 1);
-
     this.frameLandmarks();
   }
 
@@ -113,11 +88,9 @@ export default class HelloMediaPipe {
       [0, 0, 0], [0, -VIDEO_HEIGHT, 0],
       [-VIDEO_WIDTH, 0, 0], [-VIDEO_WIDTH, -VIDEO_HEIGHT, 0]
     ];
-    this.context.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
     const predictions = await this.model.estimateHands(this.video);
     if (predictions.length > 0) {
       const result = predictions[0].landmarks;
-      this.drawKeyPoints(result);
 
       if (this.scatterGL) {
         const pointsData = result.map(point => {
@@ -144,37 +117,5 @@ export default class HelloMediaPipe {
       }
     }
     requestAnimationFrame(() => this.frameLandmarks());
-  }
-
-  drawKeyPoints(keyPoints: Coords3D) {
-    const keypointArray = keyPoints;
-    for (let i = 0; i < keypointArray.length; i++) {
-      const y = keypointArray[i][0];
-      const x = keypointArray[i][1];
-      this.drawPoint(x - 2, y - 2, 3);
-    }
-
-    const fingers = Object.keys(fingerLookupIndices);
-    for (let i = 0; i < fingers.length; i++) {
-      const finger = fingers[i];
-      const points = fingerLookupIndices[finger].map(idx => keyPoints[idx]);
-      this.drawPath(points);
-    }
-  }
-
-  drawPoint(y: number, x: number, r: number) {
-    this.context.beginPath();
-    this.context.arc(x, y, r, 0, 2 * Math.PI);
-    this.context.fill();
-  }
-
-  drawPath(points: number[]) {
-    const region = new Path2D();
-    region.moveTo(points[0][0], points[0][1]);
-    for (let i = 1; i < points.length; i++) {
-      const point = points[i];
-      region.lineTo(point[0], point[1]);
-    }
-    this.context.stroke(region);
   }
 }
